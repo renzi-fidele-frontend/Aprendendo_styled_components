b132086f95940e42395aaa13d9070ea722170b8c
Renzi Fidele <renzifidele2001@gmail.com>
2023-11-15 01:23:42 +0200

    Criando primeiro componente local

--- Styled-components/src/App.jsx
+++ Styled-components/src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import styled from "styled-components";
+
+const Botao1 = styled.button`
+   border: 1px solid green;
+   background-color: greenyellow;
+   font-size: 30px;
+   margin: 1em;
+   padding: 10px 1em;
+`;
 
 function App() {
-   return <></>;
+   return (
+      <div id="App">
+         <button>Botão default</button>
+         <Botao1>Botao1</Botao1>
+
+      </div>
+   );
 }
 
 export default App;
